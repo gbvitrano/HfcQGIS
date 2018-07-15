cafc32090d28436349892b93153c987eaf728cbd
Andrea Borruso <aborruso@gmail.com>
2018-07-15 11:42:46 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -135,6 +135,6 @@ def setup(app):
     app.add_config_value('recommonmark_config', {
         'url_resolver': lambda url: github_doc_root + url,
         'enable_eval_rst': True,
-        'enable_auto_doc_ref': True,
+        'enable_auto_doc_ref': True
     }, True)
     app.add_transform(AutoStructify)
