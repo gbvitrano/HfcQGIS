62bcc29923071a3d01eb665de797d44771e2da5f
Andrea Borruso <aborruso@gmail.com>
2018-07-18 18:52:23 +0200

    rimosso nota di copyright

--- conf.py
+++ conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # project = u'HfcQGIS'
 project = u''
-copyright = u'2018, Salvatore Fiandaca - RTD by @opendatasicilia'
+copyright = u''
 author = u'Salvatore Fiandaca'
 
 # The version info for the project you're documenting, acts as replacement for
